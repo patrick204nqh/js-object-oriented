51fcbfb9d35185882e0df3ee75350a187289c28d
patrick <patrick204nqh@gmail.com>
2020-07-07 10:55:51 +0700

    class constructors

---
+++ README.md
@@ -0,0 +1,4 @@
+# Patrick's Project ( using Object Oriented JavaScript)
+
+1. Source video
+   > https://www.youtube.com/watch?v=HboT8g_QSGc&list=PL4cUxeGkcC9i5yvDkJgt60vNVWffpblB7&index=5
--- objects.js
+++ objects.js
@@ -1,12 +1,17 @@
-var userOne = {
-  email: 'patrick@dev.com',
-  name: 'Patrick',
-  login() {
-    console.log(this.email, ' has logged in');
-  },
-  logout() {
-    console.log(this.email, ' has logged out');
-  },
-};
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
+}
 
-console.log(userOne.name);
+var userOne = new User('patrick@dev.com', 'Patrick');
+var userTwo = new User('robert@dev.com', 'Robert');
+
+console.log(userOne);
+console.log(userTwo);
+
+// the 'new' keyword
+// - creates a new empty object {}
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
