be941ba19f50e76dbe7ba233e776c771d23483c1
patrick <patrick204nqh@gmail.com>
2020-07-07 12:55:33 +0700

    prototype

--- objects.js
+++ objects.js
@@ -2,11 +2,17 @@ function User(email, name) {
   this.email = email;
   this.name = name;
   this.online = false;
-  this.login = function () {
-    console.log(this.email, ' has logged in');
-  };
 }
 
+User.prototype.login = function () {
+  this.online = true;
+  console.log(this.email, ' has logged in');
+};
+
+User.prototype.logout = function () {
+  this.online = false;
+  console.log(this.email, ' has logged out');
+};
 var userOne = new User('patrick@dev.com', 'Patrick');
 var userTwo = new User('robert@dev.com', 'Robert');
 
