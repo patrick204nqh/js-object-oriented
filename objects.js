2703bd09bdc5040170d59e7740c7d10492285fe0
patrick <patrick204nqh@gmail.com>
2020-07-07 10:20:34 +0700

    object literals

--- objects.js
+++ objects.js
@@ -1,30 +1,12 @@
-// create user one
-var userOneEmail = 'patrick@dev.com';
-var userOneName = 'Patrick';
-var userOneFriends = ['Kevin'];
+var userOne = {
+  email: 'patrick@dev.com',
+  name: 'Patrick',
+  login() {
+    console.log(this.email, ' has logged in');
+  },
+  logout() {
+    console.log(this.email, ' has logged out');
+  },
+};
 
-// create user two
-var userTwoEmail = 'robert@dev.com';
-var userTwoName = 'Robert';
-var userTwoFriends = ['Kevin'];
-
-// create user one
-var userThreeEmail = 'kevin@dev.com';
-var userThreeName = 'Kevin';
-var userThreeFriends = ['Patrick', 'Robert'];
-
-function login(email) {
-  console.log(email, 'is now online');
-}
-
-function logout(email) {
-  console.log(email, 'has logged out');
-}
-
-function logFriends(friends) {
-  friends.forEach((friend) => {
-    console.log(friend);
-  });
-}
-
-login(userOneEmail);
+console.log(userOne.name);
