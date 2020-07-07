fbe22e75001476cb1f9d81c4c022d82ac5f39eed
patrick <patrick204nqh@gmail.com>
2020-07-07 12:33:48 +0700

    class inheritance

--- objects.js
+++ objects.js
@@ -2,17 +2,36 @@ class User {
   constructor(email, name) {
     this.email = email;
     this.name = name;
+    this.score = 0;
   }
   login() {
     console.log(this.email, ' just logged in');
+    return this;
   }
   logout() {
     console.log(this.email, ' just logged out');
+    return this;
+  }
+  updateScore() {
+    this.score++;
+    console.log(this.email, ' score is now ', this.score);
+    return this;
+  }
+}
+
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter((u) => u.email != user.email);
   }
 }
 
 var userOne = new User('patrick@dev.com', 'Patrick');
 var userTwo = new User('robert@dev.com', 'Robert');
+var admin = new Admin('kevin.@gmail.com', 'Kevin');
 
-userOne.login();
-userTwo.logout();
+var users = [userOne, userTwo, admin];
+
+// admin.deleteUser(userOne);
+// userOne.deleteUser(userTwo);
+
+console.log(users);
