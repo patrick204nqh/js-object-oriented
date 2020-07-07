8d716b0a07c47ef4a5162a5e3c72b8f2b9c88e28
patrick <patrick204nqh@gmail.com>
2020-07-07 13:12:01 +0700

    prototype inheritance

--- objects.js
+++ objects.js
@@ -13,8 +13,23 @@ User.prototype.logout = function () {
   this.online = false;
   console.log(this.email, ' has logged out');
 };
+
+function Admin(...args) {
+  // console.log(args);
+  User.apply(this, args);
+  this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function (user) {
+  users = users.filter((u) => u.email !== user.email);
+};
+
 var userOne = new User('patrick@dev.com', 'Patrick');
 var userTwo = new User('robert@dev.com', 'Robert');
+var admin = new Admin('kevin@dev.com', 'Kevin');
 
-console.log(userOne);
-userTwo.login();
+var users = [userOne, userTwo, admin];
+// console.log(userOne);
+// userTwo.login();
