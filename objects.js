0e6e3b4ed027010db76e1c682267429b5524d3e9
patrick <patrick204nqh@gmail.com>
2020-07-07 12:40:07 +0700

    constructors (under the hood)

--- objects.js
+++ objects.js
@@ -1,37 +1,14 @@
-class User {
-  constructor(email, name) {
-    this.email = email;
-    this.name = name;
-    this.score = 0;
-  }
-  login() {
-    console.log(this.email, ' just logged in');
-    return this;
-  }
-  logout() {
-    console.log(this.email, ' just logged out');
-    return this;
-  }
-  updateScore() {
-    this.score++;
-    console.log(this.email, ' score is now ', this.score);
-    return this;
-  }
-}
-
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter((u) => u.email != user.email);
-  }
+function User(email, name) {
+  this.email = email;
+  this.name = name;
+  this.online = false;
+  this.login = function () {
+    console.log(this.email, ' has logged in');
+  };
 }
 
 var userOne = new User('patrick@dev.com', 'Patrick');
 var userTwo = new User('robert@dev.com', 'Robert');
-var admin = new Admin('kevin.@gmail.com', 'Kevin');
 
-var users = [userOne, userTwo, admin];
-
-// admin.deleteUser(userOne);
-// userOne.deleteUser(userTwo);
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
